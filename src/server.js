0a768aee5f72324ee015873a0c8b34b43aecd93e
Bridget McErlean <bridgetmcerlean@gmail.com>
2015-10-25 13:33:46 +0000

    Make test pass by creating an express server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,12 @@
 var express = require('express');
 
-module.exports = function() {
+module.exports = function(port) {
+    var app = express();
 
+    return new Promise(function(resolve, reject) {
+        var server = app.listen(port, function () {
+            resolve(server);
+        });
+        server.on('error', reject);
+    });
 };
