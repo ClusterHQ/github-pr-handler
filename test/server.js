5ca5d668a6ad2db27119e6aa98637e2b5f89df61
Bridget McErlean <bridgetmcerlean@gmail.com>
2015-10-25 13:16:32 +0000

    Add failing test to show that server should respond with 404

--- test/server.js
+++ test/server.js
@@ -1,5 +1,19 @@
+var rp = require('request-promise');
+var server = require('../src/server');
+var expect = require('chai').expect;
+
 describe('server', function() {
-    it('does something', function() {
-       
+    it('responds with 404', function() {
+        var port = 8081;
+        server(port);
+        var options = {
+            uri: 'http://localhost:' + port,
+            simple: false,
+            resolveWithFullResponse: true
+        };
+        return rp(options)
+            .then(function(response) {
+                expect(response.statusCode).to.equal(404);
+            });
     });
 });
