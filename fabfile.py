5a6df97a65ada8a9b2d9c729ee66275663fe027c
Bridget McErlean <bridgetmcerlean@gmail.com>
2015-10-27 14:20:55 +0000

    Create up/down tasks for managing the instance

--- fabfile.py
+++ fabfile.py
@@ -2,59 +2,11 @@ import os
 
 from fabric.api import task, env
 from bookshelf.api_v1 import (create_server,
-                              load_state_from_disk)
-
-
-class UbuntuEC2CloudInstance():
-    def __init__(self, instance_name, description, tag):
-        self._initialise_from_env()
-        self._configure(instance_name, description, tag)
-
-    def _initialise_from_env(self):
-        self._instance_type = os.getenv('AWS_INSTANCE_TYPE', 't2.medium')
-        self._key_filename = os.environ['AWS_KEY_FILENAME']  # path to ssh key
-        self._key_pair = os.environ['AWS_KEY_PAIR']
-        self._region = os.getenv('AWS_REGION', 'us-west-2')
-        self._secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
-        self._access_key_id = os.environ['AWS_ACCESS_KEY_ID']
-        self._key_filename = os.environ['AWS_KEY_FILENAME']
-
-
-    def _configure(self, instance_name, description, tag):
-        self._config = {
-            'ami': 'ami-87bea5b7',
-            'username': 'ubuntu',
-            'disk_name': '/dev/sda1',
-            'disk_size': '40',
-            'instance_type': self._instance_type,
-            'key_pair': self._key_pair,
-            'region': self._region,
-            'secret_access_key': self._secret_access_key,
-            'access_key_id': self._access_key_id,
-            'security_groups': ['ssh'],
-            'instance_name': instance_name,
-            'description': description,
-            'key_filename': self._key_filename,
-            'tags': {'name': tag}
-        }
-
-    def start(self):
-        env.user = self._config['username']
-        env.key_filename = self._config['key_filename']
-        create_server(cloud = 'ec2',
-                      region = self._config['region'],
-                      access_key_id = self._config['access_key_id'],
-                      secret_access_key = self._config['secret_access_key'],
-                      distribution = 'ubuntu14.04',
-                      disk_name = self._config['disk_name'],
-                      disk_size = self._config['disk_size'],
-                      ami = self._config['ami'],
-                      key_pair = self._config['key_pair'],
-                      instance_type = self._config['instance_type'],
-                      instance_name = self._config['instance_name'],
-                      username = self._config['username'],
-                      security_groups = self._config['security_groups'],
-                      tags = self._config['tags'])
+                              down as f_down,
+                              ec2,
+                              is_there_state,
+                              load_state_from_disk,
+                              up as f_up)
 
 
 class MyCookbooks():
@@ -68,17 +20,28 @@ class MyCookbooks():
         ]
 
     def start_github_handler_instance(self):
-        instance_name = 'github_pr_handler'
-        description = \
-            'Receive requests from Github and trigger the appropriate jobs'
-        tag = 'github_pr_handler'
-        instance = UbuntuEC2CloudInstance(instance_name, description, tag)
-        instance.start()
-
+        pass
         # install git
         # clone package
         # install docker
         # start docker container with relevant parameters
+
+cloud_config = {
+    'ami': 'ami-87bea5b7',
+    'username': 'ubuntu',
+    'disk_name': '/dev/sda1',
+    'disk_size': '40',
+    'instance_type': os.getenv('AWS_INSTANCE_TYPE', 't2.medium'),
+    'key_pair': os.environ['AWS_KEY_PAIR'],
+    'region': os.getenv('AWS_REGION', 'us-west-2'),
+    'secret_access_key': os.environ['AWS_SECRET_ACCESS_KEY'],
+    'access_key_id': os.environ['AWS_ACCESS_KEY_ID'],
+    'security_groups': ['ssh'],
+    'instance_name': 'github_pr_handler',
+    'description': 'Receive requests from Github and triggers jobs on Jenkins',
+    'key_filename': os.environ['AWS_KEY_FILENAME'],
+    'tags': {'name': 'github_pr_handler'}
+}
 
 @task(default=True)
 def help():
@@ -87,22 +50,18 @@ def help():
           """)
 
 @task
-def down(cloud=None):
+def down():
     """ halt an existing instance """
     data = load_state_from_disk()
     region = data['region']
     cloud_type = data['cloud_type']
     distribution = data['distribution'] + data['os_release']['VERSION_ID']
-    access_key_id = C[cloud_type][distribution]['access_key_id']
-    secret_access_key = C[cloud_type][distribution]['secret_access_key']
+    access_key_id = cloud_config['access_key_id']
+    secret_access_key = cloud_config['secret_access_key']
     instance_id = data['id']
-    env.key_filename = C[cloud_type][distribution]['key_filename']
+    env.key_filename = cloud_config['key_filename']
 
-    cookbook = MyCookbooks()
-    if data['cloud_type'] == 'ec2':
-        cookbook.ec2()
-    if data['cloud_type'] == 'rackspace':
-        cookbook.rackspace()
+    ec2()
     f_down(cloud=cloud_type,
            instance_id=instance_id,
            region=region,
@@ -110,7 +69,56 @@ def down(cloud=None):
            secret_access_key=secret_access_key)
 
 @task
+def up():
+    """ boots a new instance on amazon or rackspace
+    """
+
+    if is_there_state():
+        data = load_state_from_disk()
+        cloud_type = data['cloud_type']
+        username = data['username']
+        distribution = data['distribution'] + data['os_release']['VERSION_ID']
+        region = data['region']
+        access_key_id = cloud_config['access_key_id']
+        secret_access_key = cloud_config['secret_access_key']
+        instance_id = data['id']
+        env.user = data['username']
+        env.key_filename = cloud_config['key_filename']
+
+        ec2()
+
+        f_up(cloud=cloud_type,
+             region=region,
+             instance_id=instance_id,
+             access_key_id=access_key_id,
+             secret_access_key=secret_access_key,
+             username=username)
+    else:
+        env.user = cloud_config['username']
+        env.key_filename = cloud_config['key_filename']
+
+        # no state file around, lets create a new VM
+        # and use defaults values we have in our 'cloud_config' dictionary
+        create_server(cloud='ec2',
+                      region=cloud_config['region'],
+                      access_key_id=cloud_config['access_key_id'],
+                      secret_access_key=cloud_config[
+                          'secret_access_key'],
+                      distribution='ubuntu14.04',
+                      disk_name=cloud_config['disk_name'],
+                      disk_size=cloud_config['disk_size'],
+                      ami=cloud_config['ami'],
+                      key_pair=cloud_config['key_pair'],
+                      instance_type=cloud_config['instance_type'],
+                      instance_name=cloud_config['instance_name'],
+                      username=cloud_config['username'],
+                      security_groups=cloud_config[
+                          'security_groups'],
+                      tags=cloud_config['tags'])
+
+@task
 def it(distribution):
+    up()
     bootstrap(distribution)
 
 @task
